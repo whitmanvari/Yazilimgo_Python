0be62d9ad0c52225ef10f3451965c66c362cf66c
whitmanvari <176252248+whitmanvari@users.noreply.github.com>
2026-04-19 09:34:45 +0300

    Kullanici entitysini oluşturdum. Ef core'daki [Key] ve [Required] attribute'ları yerine SQLAlchemy'nin mapped_column yapısını kullandım.  ICollection<T> yerine navigasyon propertylerini Pythonda Mapped[list[".."]] ve relationship ile bağladım. Cascade delete ile çöp veri oluşumunu engelledim. ToString() ezme methodunu pythonda __repr__ ile yaptım.

--- Entities/kullanici.py
+++ Entities/kullanici.py
@@ -1,9 +1,29 @@
-#kullanici dataclass'ı
 from datetime import datetime, date
-from sqlalchemy import Integer
-from sqlalchemy.orm import Mapped, mapped_column
-from Entities.base import Base 
+from entities.base import Base
+from sqlalchemy import Integer, String, Date, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship 
+
 
 class Kullanici(Base):
     __tablename__ = "kullanicilar"
-    kullanici_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    kullanici_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement= True)
+    kullanici_adi: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(120), nullable=False, unique=True)
+    parola_hash: Mapped[str] = mapped_column(String(256), nullable=False)
+    toplam_xp: Mapped[int] = mapped_column(Integer, default=0 )
+    seviye: Mapped[int] = mapped_column(Integer, default=1)
+    gunluk_hedef_xp: Mapped[int] = mapped_column(Integer, default=20)
+    gun_serisi: Mapped[int] = mapped_column(Integer, default=0)
+    son_aktif_tarihi: Mapped[date | None] = mapped_column(Date, nullable=True)
+    kayit_tarihi: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    avatar_yolu: Mapped[str| None] = mapped_column(String(255), nullable=True)
+
+    # İlişkiler
+    ilerlemeler: Mapped[list["IlerlemeKaydi"]] = relationship(back_populates="kullanici", cascade="all, delete-orphan") 
+    kazanimlar:   Mapped[list["KullaniciKazanim"]] = relationship(back_populates="kullanici", cascade="all, delete-orphan")
+
+    #c#taki toString() metodunu ezmeye benzer bir şekilde, kullanıcı nesnesinin okunabilir bir temsilini sağlar.
+    def _repr__(self) -> str:
+        return f"<Kullanici {self.kullanici_adi} | Seviye {self.seviye}>"
+     #kullanılan <> işareti tamamen görsel bir tercih. Best Practise'ler arasında, "Veritabanı Objesi / Sınıf Temsili" olduğunu ilk bakışta anlamak için geleneksel olarak bu ok işaretlerini kullanırlar.
