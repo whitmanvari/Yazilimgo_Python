742f612ed617e770f2fa91925f7cd2437e0f3d03
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-05-02 12:26:24 +0300

    xp servisi için bir sınıf açtım. constructor olarak bir seviye sınırı belirledim, bu temel aldığımız xp puanlama yöntemimiz olacak. Her 100 xp'de bir 1 seviye atlasın diye baştan sınırımızı belirledik. xp_ekle methodu oluşturdum bir tane. Bu method ile kullanıcıyı buluyorum ve toplam xp'sine yeni xp kazandıysa ekleme yapıp seviye atladı mı atlamadı mı kontrolü yapıyorum ve seviye atladıysa seviyesine bir ekliyorum. Sonucunda da verileri veritabanına kaydediyorum session'u commitleyerek. Şimdi bunu main içerisinde bir kez test edeceğim.

--- bll/xp_servisi.py
+++ bll/xp_servisi.py
@@ -1,1 +1,24 @@
-#xp hesaplama, seviye atlama formülleri
+from dal.kullanici_repository import KullaniciRepository
+
+class XPServisi:
+    def __init__(self, kullanici_repo: KullaniciRepository):
+        self.kullanici_repo = kullanici_repo
+        #iş kuralı: kullanıcı her 100xp topladığında 1 seviye atlasın
+        self.SEVIYE_SINIRI = 100
+
+    def xp_ekle(self, kullanici_id: int, kazanilan_xp: int):
+        #kullanıcıya xp eklesin diye yazdığım methot, bir noktadan sonra seviye de atlatacak (100xpde bir)
+        kullanici= self.kullaniic_repo.id_ile_getir(kullanici_id)
+        if not kullanici:
+            print("Hata: Kullanıcı bulunamadı!")
+            return None
+        #xp'sini mevcut xp'sinin üzerine ekleyeceğim
+        kullanici.toplam_xp += kazanilan_xp
+        print(f"Tebrikler! {kazanilan_xp} kadar XP kazandın!")
+        #seviye atlaması için de örneğin 150 // 100 = 1 ve +1, yani bu demek ki üzerine 1 seviye ekle
+        yeni_seviye= (kullanici.toplam_xp // self.SEVIYE_SINIRI) + 1
+        if yeni_seviye >kullanici.seviye:
+            print(f"Seviye atladın! Yeni seviyen: {kullanici.seviye}")
+
+        self.kullanici_repo.session.commit()
+        return kullanici
